90c180e2081d6d3daffa4781214344cbf8c0e33a
dionysus <dionysus.cho@gmail.com>
2021-03-25 11:14:14 -0700

    [twins] refactored key layout/ rxc

--- keyboards/lemony/twins/lit/config.h
+++ keyboards/lemony/twins/lit/config.h
@@ -19,9 +19,10 @@
 
 /* MITTENS pro-micro default pinout */
 #define MATRIX_ROW_PINS { F4, D3, B1, F7 } //top-bot
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, B3 } //left-right
+// #define MATRIX_COL_PINS { B3, D4, C6, D7, E6, B4, B5 } //left-right
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4, B3 } //left-right
 #define MATRIX_ROW_PINS_RIGHT { F4, D3, B1, F7 } //top-bot
-#define MATRIX_COL_PINS_RIGHT { B3, B5, B4, E6, D7, C6, D4 } //left-right
+#define MATRIX_COL_PINS_RIGHT {  B3, D4, C6, D7, E6, B4, B5 } //left-right
 
 #define ENCODERS_PAD_A { B2 }
 #define ENCODERS_PAD_B { B6 }
@@ -52,6 +53,7 @@
   #undef  RGBLED_NUM
   #define RGBLED_NUM        (RGBLED_NUM_LEFT+RGBLED_NUM_RIGHT)
   #define RGBLED_SPLIT      { RGBLED_NUM_LEFT, RGBLED_NUM_RIGHT }
+  #define RGB_MATRIX_SPLIT RGBLED_SPLIT
   #define DRIVER_LED_TOTAL  RGBLED_NUM
   #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
   // #define RGBLIGHT_ANIMATIONS
@@ -61,7 +63,8 @@
   // #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
   // #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
   #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-  #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_DIGITAL_RAIN// Sets the default mode, if none has been set
+  // #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_CROSS// Sets the default mode, if none has been set
+  #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE_CROSS// Sets the default mode, if none has been set
   // #define RGB_MATRIX_STARTUP_HUE 0 // Sets the default hue value, if none has been set
   // #define RGB_MATRIX_STARTUP_SAT 255 // Sets the default saturation value, if none has been set
   #define RGB_MATRIX_STARTUP_VAL 60 // Sets the default brightness value, if none has been set
@@ -71,8 +74,8 @@
   #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
   #define DISABLE_RGB_MATRIX_BREATHING
   #define DISABLE_RGB_MATRIX_CYCLE_ALL
-  #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-  #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+  // #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+  // #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
   #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
   #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
   #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
@@ -81,13 +84,13 @@
   #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
   #define DISABLE_RGB_MATRIX_RAINDROPS
   #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-  #define DISABLE_RGB_MATRIX_TYPING_HEATMAP
+  // #define DISABLE_RGB_MATRIX_TYPING_HEATMAP
   // #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+  // #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
   #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
