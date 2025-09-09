d5e96e9f4d2d13a533480f50aac00b60cfbd7b2e
dionysus <dionysus.cho@gmail.com>
2025-09-09 20:46:07 +0900

    update twins and twins_midi - todo: twins led matrix assignment

--- keyboards/lemony/twins_midi/keymaps/default/keymap.c
+++ keyboards/lemony/twins_midi/keymaps/default/keymap.c
@@ -13,7 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //     KC_I, KC_J, KC_K, KC_L, KC_4, KC_5,
     //     KC_M, KC_N, KC_O, KC_P, KC_6, KC_7, KC_8),
 
-    [0] = LAYOUT(MI_C1, MI_E1, MI_Gs1, MI_C2, MI_OCTD, MI_OCTU, MI_Cs1, MI_F1, MI_A1, MI_Cs2, KC_2, KC_3, MI_D1, MI_Fs1, MI_As1, MI_D2, KC_4, KC_5, MI_Ds1, MI_G1, MI_B1, MI_Ds2, KC_6, KC_7, KC_8),
+    [0] = LAYOUT(
+        KC_ESC, MI_E1, MI_Gs1, MI_C2, MI_OCTD, MI_OCTU, 
+        MI_Cs1, MI_F1, MI_A1, MI_Cs2, KC_2, KC_3, 
+        MI_D1, MI_Fs1, MI_As1, MI_D2, KC_4, KC_5, 
+        MI_Ds1, MI_G1, MI_B1, MI_Ds2, KC_6, KC_7, KC_8),
 
     // [0] = LAYOUT(
     //   MI_C2, MI_Cs2, MI_D2, MI_Ds2, _______, _______,
